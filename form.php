c3f1b97e303f8096818c11c14e2bf837f9833b30
Lidia Morina <morina.lida@mail.ru>
2020-01-22 19:28:17 +0800

    генерация документа с расчётами в папку сервера

--- form.php
+++ form.php
@@ -1,14 +1,14 @@
 <?php 
-/*
-Видеокурс по созданию онлайн сервисов: http://aff.krotovroman.ru/phppro/
-Видеокурс по созданию продающих сайтов: http://aff.krotovroman.ru/landing/
-*/
+
+
 set_include_path(get_include_path().PATH_SEPARATOR.realpath(__DIR__."/library/")); 
 require_once 'config.php';
-function __autoload($name_class) {require_once $name_class.'.php';} //автозагрузка классов из папки
-
-$object = new stdClass(); // Создаём объект
-foreach( $_REQUEST as $key=>$val )  $object->$key = sanitize($val); //получаем переменные
+//автозагрузка классов из папки
+function __autoload($name_class) {require_once $name_class.'.php';} 
+//создание объекта
+$object = new stdClass(); 
+//получение данных из формы
+foreach( $_REQUEST as $key=>$val )  $object->$key = sanitize($val); 
 
 $word= new Word();
 
--- library/Word.php
+++ library/Word.php
@@ -1,27 +1,26 @@
 <?php
 /*
-Работа с обьектом - Word
 генерация word документов
 */	
 		
 class Word {	
 	const VERSION = 1.00;	
-	
+
 	/*
-	ПРИМЕНЕНИЕ:
 	$word=new Word();
 	$word->generate($data);
-	//решение проблемы с русскими буквами http://kumatoz.ru/novosti/reshenie-problemy-russkix-simvolov-v-phpword-setvalue/
+	
 	*/
-	public function generate($data=NULL){	//генерация документа по шаблону с переменными			
+	//генерация документа по шаблону с переменными	
+	public function generate($data=NULL){			
 		
-	if (empty($data['template'])) 
-      $template="template1";
+		if (empty($data['template'])) 
+		  $template="template1";
     
-    $tmp_patch=realpath(__DIR__."/../templates/".$template.".docx");//путь до шаблона юр лица biblio7.loc/example	
+		$tmp_patch=realpath(__DIR__."/../templates/".$template.".docx");
 		
-		if ($tmp_patch==false) {//есть ли файл шаблона на сервере?
-			echo "Шаблон договора не найден.";
+		if ($tmp_patch==false) {
+			echo "Шаблон плана не найден.";
 			return false;
 		}
 		
@@ -40,7 +39,8 @@ class Word {
 		$this->properties->setCreated(time()); //time()
 		$this->properties->setModified(time());
 		$this->properties->setSubject($this->rus2translit($subject));
-		
+		//$this->properties->setDefaultFontName('Times New Roman');
+		//$this->properties->setDefaultFontSize(14);
 		//получаем переменные для подстановки в шаблон
 		// в шаблоне - docx файле должны быть переменные вида ${fio} причем важно чтобы они вставлялись
 		// одним элементом, т.е. без пробелов и лучше просто скопировать отсюда и вставить.
@@ -52,127 +52,411 @@ class Word {
 		$data['created']		=date("d.m.Y")." года";
 		
 		if (empty($data['id_document']))
-      $data['id_document']	=uniqid();
+			$data['id_document']	=uniqid();
     
 	
 		if (empty($data['general_info'])) 	
-		$data['general_info']			="пусто";
+			$data['general_info']			="пусто";
 	
 		if (empty($data['fire-load-data'])) 	
-		$data['fire_load_data']			="пусто11";
+			$data['fire_load_data']			="пусто11";
 	
 		if (empty($data['fire_protection'])) 	
-		$data['fire_protection']			="пусто2";
+			$data['fire_protection']			="пусто2";
 	
 		if (empty($data['object_communication'])) 	
-		$data['object_communication']			="пусто23";
+			$data['object_communication']			="пусто23";
 	
 	
 		if (empty($data['inner-water-supply'])) 	
-		$data['inner-water-supply']			="пусто3";
+			$data['inner-water-supply']			="пусто3";
 	
 		if (empty($data['outer-water-supply'])) 	
-		$data['outer-water-supply']			="пусто4";
+			$data['outer-water-supply']			="пусто4";
 		
 		if (empty($data['primary-fire-extinguishing-means'])) 	
-		$data['primary-fire-extinguishing-means']			="пусто5";
+			$data['primary-fire-extinguishing-means']			="пусто5";
 	
 		if (empty($data['security_communications'])) 	
-		$data['security_communications']			="пусто6";
+			$data['security_communications']			="пусто6";
 		
 		if (empty($data['variant-1'])) 	
-		$data['variant-1']			="пусто6";
+			$data['variant-1']			="пусто6";
 	
 		if (empty($data['linear-velocity-var1'])) 	
-		$data['linear-velocity-var1']			="пусто6";
+			$data['linear-velocity-var1']			="пусто6";
 	
 		if (empty($data['intensity-var1'])) 	
-		$data['intensity-var1']			="пусто6";
+			$data['intensity-var1']			="пусто6";
 	
 		if (empty($data['variant-2'])) 	
-		$data['variant-2']			="пусто6";
+			$data['variant-2']			="пусто6";
 	
 		if (empty($data['linear-velocity-var2'])) 	
-		$data['linear-velocity-var2']			="пусто6";
+			$data['linear-velocity-var2']			="пусто6";
 	
 		if (empty($data['fire-propagation-routes'])) 	
-		$data['fire-propagation-routes']			="пусто6";
+			$data['fire-propagation-routes']			="пусто6";
 	
 		if (empty($data['possible-smoke-zones'])) 	
-		$data['possible-smoke-zones']			="пусто6";
+			$data['possible-smoke-zones']			="пусто6";
 	
 		if (empty($data['emergency-response-information'])) 	
-		$data['emergency-response-information']			="пусто6";
+			$data['emergency-response-information']			="пусто6";
 		
 		if (empty($data['building'])) 	
-		$data['building']			="пусто6";
+			$data['building']			="пусто6";
 	
 		if (empty($data['day'])) 	
-		$data['day']			="пусто6";
+			$data['day']			="пусто6";
 	
 		if (empty($data['night'])) 	
 		$data['night']			="пусто6";
 	
 		if (empty($data['exit-from-building'])) 	
-		$data['exit-from-building']			="пусто6";
+			$data['exit-from-building']			="пусто6";
 		
 		if (empty($data['time-massage-var1'])) 	
-		$data['time-massage-var1']			="пусто6";
+			$data['time-massage-var1']			="пусто6";
 	
 		if (empty($data['street'])) 	
-		$data['street']			="пусто6";
+			$data['street']			="пусто6";
 	//расчёт
 	//1
 		if (empty($data['formula_t_ds'])) 	
-		$data['formula_t_ds']			="пусто6";
+			$data['formula_t_ds']			="пусто6";
 	
 		if (empty($data['formula_t_sb'])) 	
-		$data['formula_t_sb']			="пусто6";
+			$data['formula_t_sb']			="пусто6";
 		
 		if (empty($data['formula_t_sl'])) 	
-		$data['formula_t_sl']			="пусто6";
+			$data['formula_t_sl']			="пусто6";
 		
 		if (empty($data['formula_t_br1'])) 	
-		$data['formula_t_br1']			="пусто6";
+			$data['formula_t_br1']			="пусто6";
 	
 		if (empty($data['formula_L'])) 	
-		$data['formula_L']			="пусто6";
+			$data['formula_L']			="пусто6";
 	
 		if (empty($data['formula_Vsl'])) 	
-		$data['formula_Vsl']			="пусто6";
+			$data['formula_Vsl']			="пусто6";
 		
 		$data['formula_t_sv'] = $data['formula_t_ds'] + $data['formula_t_sb']+$data['formula_t_sl']+$data['formula_t_br1'];
 		$data['formula_t_sl'] = $data['formula_L'] * 60/$data['formula_Vsl'];
 	//2
 		if (empty($data['formula_Vl'])) 	
-		$data['formula_Vl']			="пусто6";
+			$data['formula_Vl']			="пусто6";
 	
 		$data['formula_t2'] = $data['formula_t_sv'] - 10;
 		$data['formula_R1'] = 0.5* $data['formula_Vl'] * 10+$data['formula_Vl']*$data['formula_t_sv'];
 	
 	//3	
 		if (empty($data['formula_L_way'])) 	
-		$data['formula_L_way']			="пусто путь";
-	
+			$data['formula_L_way']			="пусто путь";
+		
 		if (empty($data['formula_a_wight'])) 	
-		$data['formula_a_wight']			="пусто ширина";
+			$data['formula_a_wight']			="пусто ширина";
 
 		$data['formula_Sp'] = $data['formula_L_way'] * $data['formula_a_wight'];
 		
 	//4 	
 		if (empty($data['formula_n'])) 	
-		$data['formula_n']			="пусто путь";
+			$data['formula_n']			="пусто путь";
 	
 		if (empty($data['formula_ht'])) 	
-		$data['formula_ht']			="пусто ширина";
+			$data['formula_ht']			="пусто ширина";
 	
 		$data['formula_St'] = $data['formula_n'] * $data['formula_ht']*$data['formula_a_wight'];
+	
+	//5
+		if (empty($data['formula_Itr'])) 	
+			$data['formula_Itr']			="пусто путь";
+	
+		$data['formula_water-consumption'] = $data['formula_St'] * $data['formula_Itr'];
+
+	//6 условие доделать текста
+		if (empty($data['formula_q_stvB'])) 	
+			$data['formula_q_stvB']			="пусто путь";
+
+		$data['formula_Nt_stvB'] = $data['formula_water-consumption'] / $data['formula_q_stvB'];
+		//$data['formula_Nt_stvB_round'] = round($data['formula_Nt_stvB'],2)
+
+		$data['formula_Nt_stvB_ceil']= ceil($data['formula_Nt_stvB']);
+
+	//7
+		if (empty($data['formula_Nst_t'])) 	
+			$data['formula_Nst_t']			="пусто";
+		
+		if (empty($data['formula_qt_stB'])) 	
+			$data['formula_qt_stB']			="пусто";
+	
+		$data['formula_Qt_fact'] = $data['formula_Nst_t'] * $data['formula_qt_stB'];
+	
+	//8
+	
+	
+	//9
+		if (empty($data['formula_Nst_z'])) 	
+			$data['formula_Nst_z']			="пусто";
+		
+		if (empty($data['formula_Qz_stB'])) 	
+			$data['formula_Qz_stB']			="пусто";
+		
+		$data['formula_Qz_fact'] = $data['formula_Nst_z'] * $data['formula_Qz_stB'];
+	
+	//10
+		$data['formula_Q_fact'] = $data['formula_Qt_fact'] + $data['formula_Qz_fact'];
+	
+	
+	//11
+	
+	//12
+	
+
+	//13
+		if (empty($data['formula_Hh'])) 	
+			$data['formula_Hh']			="пусто";
+		
+		if (empty($data['formula_Hp'])) 	
+			$data['formula_Hp']			="пусто";
+		
+		if (empty($data['formula_Zm'])) 	
+			$data['formula_Zm']			="пусто";
+		
+		if (empty($data['formula_Zst'])) 	
+			$data['formula_Zst']			="пусто";
+		
+		if (empty($data['formula_S'])) 	
+			$data['formula_S']			="пусто";
+		
+		if (empty($data['formula_Q'])) 	
+			$data['formula_Q']			="пусто";
+		
+		$data['formula_Lpr'] = ($data['formula_Hh'] - ( $data['formula_Hp']+$data['formula_Zm']+$data['formula_Zst']))*20/($data['formula_S']*$data['formula_Q']*$data['formula_Q']);
+	
+	//14
+		$data['formula_Vvo'] = $data['formula_Q_fact'] *60*10;
+	
+		if (empty($data['conclusion_supplyOfwater'])) 	
+			$data['conclusion_supplyOfwater']			="пусто";
+		
+	//15
+		if (empty($data['formula_N_tush'])) 	
+			$data['formula_N_tush']			="пусто";
+		
+		if (empty($data['formula_N_zash'])) 	
+			$data['formula_N_zash']			="пусто";
+		
+		if (empty($data['formula_N_search'])) 	
+			$data['formula_N_search']			="пусто";
+		
+		if (empty($data['formula_Nrez_gdzs'])) 	
+			$data['formula_Nrez_gdzs']			="пусто";
+		
+		if (empty($data['formula_N_kpp'])) 	
+			$data['formula_N_kpp']			="пусто";
+		
+		if (empty($data['formula_N_pb'])) 	
+			$data['formula_N_pb']			="пусто";
+		
+		if (empty($data['formula_N_razv'])) 	
+			$data['formula_N_razv']			="пусто";
+		
+		if (empty($data['formula_N_sv'])) 	
+			$data['formula_N_sv']			="пусто";
+		
+		if (empty($data['formula_N_vod'])) 	
+			$data['formula_N_vod']			="пусто";
+	
+		$data['formula_Nls'] = $data['formula_N_tush'] *3 + $data['formula_N_zash'] *3 
+								+ $data['formula_N_search'] *3 + $data['formula_Nrez_gdzs'] *3 
+								+ $data['formula_N_kpp'] *1 +$data['formula_N_pb'] *1 + 
+								$data['formula_N_razv'] *1 +$data['formula_N_sv'] *1 + $data['formula_N_vod'] *1;
+	
+		
+		
+	//16
+		$data['formula_Notd'] = $data['formula_Nls'] / 5 ;
+		$data['formula_Notd_ceil'] = ceil($data['formula_Notd']) ;
+	
+		if (empty($data['conclusion'])) 	
+			$data['conclusion']			="пусто";
+	
+	
+	// тактический замысел2
+	if (empty($data['time-massage-var2'])) 	
+			$data['time-massage-var2']			="пусто6";
+	
+		if (empty($data['street-var2'])) 	
+			$data['street-var2']			="пусто6";
+	//расчёт
+	//1
+		if (empty($data['formula_t_ds-var2'])) 	
+			$data['formula_t_ds-var2']			="пусто6";
+	
+		if (empty($data['formula_t_sb-var2'])) 	
+			$data['formula_t_sb-var2']			="пусто6";
+		
+		if (empty($data['formula_t_sl-var2'])) 	
+			$data['formula_t_sl-var2']			="пусто6";
+		
+		if (empty($data['formula_t_br1-var2'])) 	
+			$data['formula_t_br1-var2']			="пусто6";
+	
+		if (empty($data['formula_L-var2'])) 	
+			$data['formula_L-var2']			="пусто6";
+	
+		if (empty($data['formula_Vsl-var2'])) 	
+			$data['formula_Vsl-var2']			="пусто6";
+		
+		$data['formula_t_sv-var2'] = $data['formula_t_ds-var2'] + $data['formula_t_sb-var2']+$data['formula_t_sl-var2']+$data['formula_t_br1-var2'];
+		$data['formula_t_sl-var2'] = $data['formula_L-var2'] * 60/$data['formula_Vsl-var2'];
+	//2
+		if (empty($data['formula_Vl-var2'])) 	
+			$data['formula_Vl-var2']			="пусто6";
+	
+		$data['formula_t2-var2'] = $data['formula_t_sv-var2'] - 10;
+		$data['formula_R1-var2'] = 0.5* $data['formula_Vl-var2'] * 10+$data['formula_Vl-var2']*$data['formula_t_sv-var2'];
+	
+	//3	
+		if (empty($data['formula_L_way-var2'])) 	
+			$data['formula_L_way-var2']			="пусто путь";
+		
+		if (empty($data['formula_a_wight-var2'])) 	
+			$data['formula_a_wight-var2']			="пусто ширина";
+
+		$data['formula_Sp-var2'] = $data['formula_L_way-var2'] * $data['formula_a_wight-var2'];
+		
+	//4 	
+		if (empty($data['formula_n-var2'])) 	
+			$data['formula_n-var2']			="пусто путь";
+	
+		if (empty($data['formula_ht-var2'])) 	
+			$data['formula_ht-var2']			="пусто ширина";
+	
+		$data['formula_St-var2'] = $data['formula_n-var2'] * $data['formula_ht-var2']*$data['formula_a_wight-var2'];
+	
+	//5
+		if (empty($data['formula_Itr-var2'])) 	
+			$data['formula_Itr-var2']			="пусто путь";
+	
+		$data['formula_water-consumption-var2'] = $data['formula_St-var2'] * $data['formula_Itr-var2'];
+
+	//6 условие доделать текста
+		if (empty($data['formula_q_stvB-var2'])) 	
+			$data['formula_q_stvB-var2']			="пусто путь";
+
+		$data['formula_Nt_stvB-var2'] = $data['formula_water-consumption-var2'] / $data['formula_q_stvB-var2'];
+		//$data['formula_Nt_stvB_round'] = round($data['formula_Nt_stvB'],2)
+
+		$data['formula_Nt_stvB_ceil-var2']= ceil($data['formula_Nt_stvB-var2']);
+
+	//7
+		if (empty($data['formula_Nst_t-var2'])) 	
+			$data['formula_Nst_t-var2']			="пусто";
+		
+		if (empty($data['formula_qt_stB-var2'])) 	
+			$data['formula_qt_stB-var2']			="пусто";
+	
+		$data['formula_Qt_fact-var2'] = $data['formula_Nst_t-var2'] * $data['formula_qt_stB-var2'];
+	
+	//8
+	
+	
+	//9
+		if (empty($data['formula_Nst_z-var2'])) 	
+			$data['formula_Nst_z-var2']			="пусто";
+		
+		if (empty($data['formula_Qz_stB-var2'])) 	
+			$data['formula_Qz_stB-var2']			="пусто";
+		
+		$data['formula_Qz_fact-var2'] = $data['formula_Nst_z-var2'] * $data['formula_Qz_stB-var2'];
+	
+	//10
+		$data['formula_Q_fact-var2'] = $data['formula_Qt_fact-var2'] + $data['formula_Qz_fact-var2'];
+	
+	
+	//11
+	
+	//12
+	
+
+	//13
+		if (empty($data['formula_Hh-var2'])) 	
+			$data['formula_Hh-var2']			="пусто";
+		
+		if (empty($data['formula_Hp-var2'])) 	
+			$data['formula_Hp-var2']			="пусто";
+		
+		if (empty($data['formula_Zm-var2'])) 	
+			$data['formula_Zm-var2']			="пусто";
+		
+		if (empty($data['formula_Zst-var2'])) 	
+			$data['formula_Zst-var2']			="пусто";
+		
+		if (empty($data['formula_S-var2'])) 	
+			$data['formula_S-var2']			="пусто";
+		
+		if (empty($data['formula_Q-var2'])) 	
+			$data['formula_Q-var2']			="пусто";
+		
+		$data['formula_Lpr-var2'] = ($data['formula_Hh-var2'] - ( $data['formula_Hp-var2']+$data['formula_Zm-var2']+$data['formula_Zst-var2']))*20/($data['formula_S-var2']*$data['formula_Q-var2']*$data['formula_Q-var2']);
+	
+	//14
+		$data['formula_Vvo-var2'] = $data['formula_Q_fact-var2'] *60*10;
+	
+		if (empty($data['conclusion_supplyOfwater-var2'])) 	
+			$data['conclusion_supplyOfwater-var2']			="пусто";
+		
+	//15
+		if (empty($data['formula_N_tush-var2'])) 	
+			$data['formula_N_tush-var2']			="пусто";
+		
+		if (empty($data['formula_N_zash-var2'])) 	
+			$data['formula_N_zash-var2']			="пусто";
+		
+		if (empty($data['formula_N_search-var2'])) 	
+			$data['formula_N_search-var2']			="пусто";
+		
+		if (empty($data['formula_Nrez_gdzs-var2'])) 	
+			$data['formula_Nrez_gdzs-var2']			="пусто";
+		
+		if (empty($data['formula_N_kpp-var2'])) 	
+			$data['formula_N_kpp-var2']			="пусто";
+		
+		if (empty($data['formula_N_pb-var2'])) 	
+			$data['formula_N_pb-var2']			="пусто";
+		
+		if (empty($data['formula_N_razv-var2'])) 	
+			$data['formula_N_razv-var2']			="пусто";
+		
+		if (empty($data['formula_N_sv-var2'])) 	
+			$data['formula_N_sv-var2']			="пусто";
+		
+		if (empty($data['formula_N_vod-var2'])) 	
+			$data['formula_N_vod-var2']			="пусто";
+	
+		$data['formula_Nls-var2'] = $data['formula_N_tush-var2'] *3 + $data['formula_N_zash-var2'] *3 
+								+ $data['formula_N_search-var2'] *3 + $data['formula_Nrez_gdzs-var2'] *3 
+								+ $data['formula_N_kpp-var2'] *1 +$data['formula_N_pb-var2'] *1 + 
+								$data['formula_N_razv-var2'] *1 +$data['formula_N_sv-var2'] *1 + $data['formula_N_vod-var2'] *1;
+	
+		
+		
+	//16
+		$data['formula_Notd-var2'] = $data['formula_Nls-var2'] / 5 ;
+		$data['formula_Notd_ceil-var2'] = ceil($data['formula_Notd-var2']) ;
+	
+		if (empty($data['conclusion-var2'])) 	
+			$data['conclusion-var2']			="пусто";
+	
+	
 		
 		//заменяем переменные	
 		foreach($data as $field=>$value)  $this->document->setValue($field, $value);		
 		
 		if (empty($data['id_document']))
-      $data['id_document']	=uniqid();
+			$data['id_document']	=uniqid();
     
   
 		//вычисляем формулы
@@ -181,7 +465,7 @@ class Word {
 		foreach($data as $field=>$value)  $this->document->setValue($field, $value);		
 		
 		 
-		if (!empty($data['server'])) {
+		if (empty($data['server'])) {
       $temp_file="finish/dogovor_".$data['id_document'].".docx";
       $this->document->saveAs($temp_file); //сохранить в временную папку на сервере	    
 		} else {
@@ -190,12 +474,12 @@ class Word {
       $this->document->saveAs($temp_file); //сохранить в временную папку на сервере		    
       //заголовки чтобы скачать сразу файл
       //header ( "Expires: Mon, 1 Apr 1974 05:00:00 GMT" );
-     /* header ( "Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT" );
+      header ( "Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT" );
       header ( "Cache-Control: no-cache, must-revalidate" );
       header ( "Pragma: no-cache" );
       header ( "Content-type: application/vnd.openxmlformats-officedocument.wordprocessingml.document" );
       header("Content-Disposition: attachment; filename=dogovor_".$data['id_document'].".docx");
-      readfile($temp_file); */
+      readfile($temp_file); 
       unlink($temp_file);
 		} 	
 			
@@ -235,6 +519,7 @@ class Word {
 		require_once (realpath(__DIR__."/phpword/Autoloader.php"));
 		\PhpOffice\PhpWord\Autoloader::register();
 		$this->phpword = new  \PhpOffice\PhpWord\PhpWord();
+		
 	}
 
 } //class
